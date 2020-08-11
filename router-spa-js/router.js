f29afafc177f8de667cd6cde92e12a347f9bda6e
Guillermo Moreno <contact@memobrown.com>
2020-08-11 11:09:36 -0500

    Haciendo match entre url y ruta

--- router-spa-js/router.js
+++ router-spa-js/router.js
@@ -3,11 +3,27 @@ class Router {
         this.routes = routes;
         this._loadInitialRoute();
     }
-}
 
-_loadInitialRoute(){
-    const pathNameSplit = window.location.pathname.split('/');
-    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1): '';
+    _matchUrlToRoute(urlSegs){
+        const matchedRoute = this.routes.find(route => {
+            const routePathSegs = route.path.split('/').slice(1)
 
-    this.loadRoute(...pathSegs)
+            if(routePathSegs.length !== urlSegs.length){
+                return false;
+            }
+
+            return routePathSegs
+                .every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+        });
+
+        return matchedRoute;
+
+    }
+
+    _loadInitialRoute(){
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1): '';
+
+        this.loadRoute(...pathSegs);
+    }
 }
