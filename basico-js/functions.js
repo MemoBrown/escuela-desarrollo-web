65ab4c0600cc3ea03062042ac476723a631b825f
Guillermo Moreno <guillermo@preyhq.com>
2022-07-20 19:40:42 -0500

    functions

---
+++ basico-js/functions.js
@@ -0,0 +1,12 @@
+// Declarativas
+function myFunction() {
+    return 3;
+}
+myFunction()
+
+// Expresion/Anonimas
+var suma = function(a, b) {
+    var resultado = a + b;
+    return resultado;
+}
+suma(1, 2)
