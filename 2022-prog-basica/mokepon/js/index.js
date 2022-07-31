ba4f3b935a5f8d8d4526ace88daa4802e75b362e
Guillermo Moreno <guillermo@preyhq.com>
2022-07-31 14:33:31 -0500

    forEach

--- 2022-prog-basica/mokepon/js/index.js
+++ 2022-prog-basica/mokepon/js/index.js
@@ -1,18 +1,10 @@
 const hideRestartButton = document.getElementById('restart')
 const showSelectAttack = document.getElementById('select-attack')
 const petButtonPlayer = document.getElementById('pet-button')
-const fireBtn = document.getElementById('fire-btn')
-const waterBtn = document.getElementById('water-btn')
-const earthBtn = document.getElementById('earth-btn')
+
 const restartBtn = document.getElementById('restart-button')
 
 const hideSelectPet = document.getElementById('select-pet')
-const inputHipodoge = document.getElementById('hipodoge')
-const inputCapipepo = document.getElementById('capipepo')
-const inputRatigueya = document.getElementById('ratigueya')
-const inputLangostelvis = document.getElementById('langostelvis')
-const inputTucapalma = document.getElementById('tucapalma')
-const inputPydos = document.getElementById('pydos')
 const petSelected = document.getElementById('pet-selected')
 
 const petSelectedEnemy = document.getElementById('enemy-pet-selected')
@@ -26,9 +18,26 @@ const newEnemyAttacks = document.getElementById("enemy-attacks")
 
 const showRestartButton = document.getElementById('restart')
 
+const form = document.getElementById('form')
+const contenedorAtaques = document.getElementById('contenedor-ataques')
+
 let mokepones = []
-let playerAttack
-let enemyAttack
+let playerAttack = []
+let enemyAttack = []
+let opcionDeMokepones
+let inputHipodoge
+let inputCapipepo
+let inputRatigueya
+let inputLangostelvis
+let inputTucapalma
+let inputPydos
+let petPlayer
+let ataquesMokepon
+let ataquesMokeponEnemigo
+let fireBtn
+let waterBtn
+let earthBtn
+let botones = []
 let playerLifes = 6
 let enemyLifes = 6
 
@@ -37,6 +46,7 @@ class Mokepon {
     this.nombre = nombre
     this.foto = foto
     this.vida = vida
+    this.ataques = []
   }
 }
 
@@ -47,15 +57,77 @@ let langostelvis = new Mokepon('Langostelvis', null, 5)
 let tucapalma = new Mokepon('Tucapalma', null, 5)
 let pydos = new Mokepon('Pydos', null, 5)
 
+hipodoge.ataques.push(
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'}
+)
+capipepo.ataques.push(
+  {nombre: 'Tierra', id: 'earth-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'}
+)
+ratigueya.ataques.push(
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'}
+)
+langostelvis.ataques.push(
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'}
+)
+tucapalma.ataques.push(
+  {nombre: 'Tierra', id: 'earth-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'}
+)
+pydos.ataques.push(
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Fuego', id: 'fire-btn'},
+  {nombre: 'Agua', id: 'water-btn'},
+  {nombre: 'Tierra', id: 'earth-btn'}
+)
+
 mokepones.push(hipodoge, capipepo, ratigueya, langostelvis, tucapalma, pydos)
 
+
 function startGame() {
+  
+  mokepones.forEach((mokepon) => {
+    opcionDeMokepones = `
+    <label for=${mokepon.nombre}>
+        <input type="radio" name="pet" id=${mokepon.nombre} class="nes-radio is-dark" checked>
+        <span>${mokepon.nombre}</span>
+      </label>
+      <br />
+      `
+      form.innerHTML += opcionDeMokepones
+
+      inputHipodoge = document.getElementById('Hipodoge')
+      inputCapipepo = document.getElementById('Capipepo')
+      inputRatigueya = document.getElementById('Ratigueya')
+      inputLangostelvis = document.getElementById('Langostelvis')
+      inputTucapalma = document.getElementById('Tucapalma')
+      inputPydos = document.getElementById('Pydos')
+
+    })
+    
   hideRestartButton.style.display = 'none'
   showSelectAttack.style.display = 'none'
   petButtonPlayer.addEventListener('click', selectPetPlayer)
-  fireBtn.addEventListener('click', fireAttack)
-  waterBtn.addEventListener('click', waterAttack)
-  earthBtn.addEventListener('click', earthAttack)
+
   restartBtn.addEventListener('click', restartGame)
 }
 
@@ -64,75 +136,108 @@ function selectPetPlayer() {
   showSelectAttack.style.display = 'block'
 
   if(inputHipodoge.checked == true) {
-    petSelected.innerHTML = "Hipodoge"
+    petSelected.innerHTML = inputHipodoge.id
+    petPlayer = inputHipodoge.id
   } else if(inputCapipepo.checked == true) {
-    petSelected.innerHTML = "Capipepo"
+    petSelected.innerHTML = inputCapipepo.id
+    petPlayer = inputCapipepo.id
   } else if(inputRatigueya.checked == true) {
-    petSelected.innerHTML = "Ratigueya"
+    petSelected.innerHTML = inputRatigueya.id
+    petPlayer = inputRatigueya.id
   } else if(inputLangostelvis.checked == true) {
-    petSelected.innerHTML = "Langostelvis"
+    petSelected.innerHTML = inputLangostelvis.id
+    petPlayer = inputLangostelvis.id
   } else if(inputTucapalma.checked == true) {
-    petSelected.innerHTML = "Tucapalma"
+    petSelected.innerHTML = inputTucapalma.id
+    petPlayer = inputTucapalma.id
   } else if(inputPydos.checked == true) {
-    petSelected.innerHTML = "Pydos"
+    petSelected.innerHTML = inputPydos.id
+    petPlayer = inputPydos.id
   } else {
     alert('Selecciona una mascota')
   }
 
+  extraerAtaques(petPlayer)
   selectPetPlayerEnemy()
 }
 
-function selectPetPlayerEnemy() {
-  let randomPet = random(1, 6)
-
-  if(randomPet == 1) {
-    petSelectedEnemy.innerHTML = "Hipodoge"
-  } else if(randomPet == 2) {
-    petSelectedEnemy.innerHTML = "Capipepo"
-  } else if(randomPet == 3) {
-    petSelectedEnemy.innerHTML = "Ratigueya"
-  } else if(randomPet == 4) {
-    petSelectedEnemy.innerHTML = "Langostelvis"
-  } else if(randomPet == 5) {
-    petSelectedEnemy.innerHTML = "Tucapalma"
-  } else {
-    petSelectedEnemy.innerHTML = "Pydos"
+function extraerAtaques(petPlayer) {
+  let ataques
+  for (let i = 0; i < mokepones.length; i++) {
+    if (petPlayer === mokepones[i].nombre) {
+      ataques = mokepones[i].ataques
+    }
   }
+  mostrarAtaques(ataques)
+}
+
+function mostrarAtaques(ataques) {
+  ataques.forEach((ataque) => {
+    ataquesMokepon = `
+      <button id=${ataque.id} class="nes-btn attack-btn">${ataque.nombre}</button>
+    `
+    contenedorAtaques.innerHTML += ataquesMokepon
+  })
+  fireBtn = document.getElementById('fire-btn')
+  waterBtn = document.getElementById('water-btn')
+  earthBtn = document.getElementById('earth-btn')
+  botones = document.querySelectorAll('.attack-btn')
 
 }
 
-function fireAttack() {
-  playerAttack = "Fuego 🔥"
-  enemyRandomAttack()
+function secuenciaAtaque() {
+  botones.forEach((boton) => {
+    boton.addEventListener('click', (e) => {
+      if (e.target.textContent == 'Fuego') {
+        playerAttack.push('Fuego 🔥')
+        console.log(playerAttack)
+        boton.classList.add('is-error')
+      } else if(e.target.textContent == 'Agua') {
+        playerAttack.push('Agua 💧')
+        console.log(playerAttack)
+        boton.classList.add('is-primary')
+      } else {
+        playerAttack.push('Tierra 🌱')
+        console.log(playerAttack)
+        boton.classList.add('is-success')
+      }
+      enemyRandomAttack()
+    })
+  })
 }
 
-function waterAttack() {
-  playerAttack = "Agua 💧"
-  enemyRandomAttack()
-}
-
-function earthAttack() {
-  playerAttack = "Tierra 🌱"
-  enemyRandomAttack()
+function selectPetPlayerEnemy() {
+  let randomPet = random(0, mokepones.length - 1)
+  petSelectedEnemy.innerHTML = mokepones[randomPet].nombre
+  ataquesMokeponEnemigo = mokepones[randomPet].ataques
+  secuenciaAtaque()
 }
 
 function enemyRandomAttack() {
-  let enemyRandomAttack = random(1,3)
+  let enemyRandomAttack = random(0, ataquesMokeponEnemigo.length -1)
 
-  if(enemyRandomAttack == 1) {
-    enemyAttack = "Fuego 🔥"
-
-  } else if(enemyRandomAttack == 2) {
-    enemyAttack = "Agua 💧"
-
+  if(enemyRandomAttack == 0 || enemyRandomAttack == 1) {
+    enemyAttack.push("Fuego 🔥")
+  } else if(enemyRandomAttack == 3 || enemyRandomAttack == 4) {
+    enemyAttack.push("Agua 💧")
   } else {
-    enemyAttack = "Tierra 🌱"
+    enemyAttack.push("Tierra 🌱")
   }
+  console.log(enemyAttack)
+  startFight()
+}
 
-  kombat()
+function startFight() {
+  if(playerAttack.length === 5) {
+    kombat()
+  }
 }
 
 function kombat() {
+  for (let index = 0; index < playerAttack.length; index++) {
+    console.log(playerAttack[index])
+  }
+
   if (enemyAttack == playerAttack) {
     createMessage("Empate 😐")
   } else if (playerAttack == 'Fuego 🔥' && enemyAttack == 'Tierra 🌱') {
