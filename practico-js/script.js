bd10d7842ba19d811ab3160c5ff2c63c1c265d10
Guillermo Moreno <guillermolpzm@gmail.com>
2022-10-17 20:32:20 -0500

    addEventListener

--- practico-js/script.js
+++ practico-js/script.js
@@ -3,9 +3,13 @@ const input1 = document.getElementById("calculo1")
 const input2 = document.getElementById("calculo2")
 const btn = document.getElementById("btn-calcular")
 const resultado = document.getElementById("resultado")
+const form = document.getElementById("form")
 
+form.addEventListener('submit', sumNum);
 
-function btnOclick() {
+function sumNum(event) {
+  // console.log({event});
+  event.preventDefault();
   if(input1.value === "" && input2.value === "") {
     resultado.innerHTML = "Escribe un numero"
   } else {
