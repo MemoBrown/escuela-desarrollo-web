11e1405bc01a53c59b00d4692a61f37050b1ad28
Guillermo Moreno <guillermolpzm@gmail.com>
2022-11-08 19:27:41 -0600

    Math.PI

--- taller-math-js/math.js
+++ taller-math-js/math.js
@@ -24,3 +24,30 @@ function calcularTriangulo(lado1, lado2, base, altura) {
     area: (base * altura) / 2,
   }
 }
+
+console.group("Circle")
+
+const radioCirculo = 3
+const diametroCirculo = radioCirculo * 2
+// const PI = 3.1415
+
+const circunferencia = diametroCirculo * PI
+const areaCirculo = (radioCirculo ** 2) * PI
+
+console.log({
+  radioCirculo,
+  diametroCirculo,
+  circunferencia,
+  areaCirculo
+});
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2
+  const radioAlCuadrado = Math.pow(radio, 2)
+  return {
+    circunferencia: diametro * Math.PI.toFixed(2),
+    area: radioAlCuadrado * Math.PI.toFixed(2)
+  }
+}
+
+console.groupEnd("Circle")
