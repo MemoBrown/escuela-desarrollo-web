c025ee54b4d90306a281c8926024a70357fd20a7
Guillermo Moreno <guillermolpzm@gmail.com>
2023-03-14 12:55:10 -0600

    find and filter method

--- taller-math-js/descuentos.js
+++ taller-math-js/descuentos.js
@@ -5,13 +5,35 @@ const result = document.getElementById("result")
 
 btnCalculate.addEventListener("click", calculateDiscountPrice)
 
-const couponsObj = {
-  "cupon10": 10,
-  "cupon20": 20,
-  "cupon30": 30,
-  "cupon50": 50,
-  "cupon70": 70
-}
+// const couponsObj = {
+//   "cupon10": 10,
+//   "cupon20": 20,
+//   "cupon30": 30,
+//   "cupon50": 50,
+//   "cupon70": 70
+// }
+
+const couponsLits = []
+couponsLits.push({
+  name: "cupon10",
+  discount: 10
+})
+couponsLits.push({
+  name: "cupon20",
+  discount: 20
+})
+couponsLits.push({
+  name: "cupon30",
+  discount: 30
+})
+couponsLits.push({
+  name: "cupon50",
+  discount: 50
+})
+couponsLits.push({
+  name: "cupon70",
+  discount: 70
+})
 
 function calculateDiscountPrice() {
   // (P * (100 - D)) / 100
@@ -34,8 +56,14 @@ function calculateDiscountPrice() {
     return
   }
 
-  if (couponsObj[coupon]) {
-    discount = couponsObj[coupon]
+  function isCouponInArray(couponElement) {
+    return couponElement.name == coupon
+  }
+
+  const couponFilter = couponsLits.filter(isCouponInArray)
+
+  if (couponFilter.length > 0) {
+    discount = couponFilter[0].discount
   } else {
     Swal.fire({
       title: 'Advertencia',
@@ -49,6 +77,22 @@ function calculateDiscountPrice() {
     })
     return
   }
+
+  // if (couponsObj[coupon]) {
+  //   discount = couponsObj[coupon]
+  // } else {
+  //   Swal.fire({
+  //     title: 'Advertencia',
+  //     text: 'El cupon ingresado no es valido',
+  //     icon: 'warning',
+  //     iconColor: "#D59A36",
+  //     confirmButtonColor: "#D59A36",
+  //     color: "#021026",
+  //     allowOutsideClick: false,
+  //     confirmButtonText: 'Volver a intentar'
+  //   })
+  //   return
+  // }
   const newPrice = (price * (100 - discount)) / 100
 
   result.innerText = `El nuevo precio con descuento es $${newPrice}`
