889a9ff8fcc7485fde5f99756e5ca105fdf2dd73
Guillermo Moreno <guillermolpzm@gmail.com>
2023-07-04 21:16:15 -0600

    slice method

--- taller-math-js/average.js
+++ taller-math-js/average.js
@@ -12,10 +12,33 @@ function calcularPromedio(lista) {
   // const elementsSum = (valorAcumulado, valorActual) => {
   //   return valorAcumulado + valorActual
   // }
-  const sumaLista = lista.reduce((valorAcumulado, valorActual) => valorAcumulado + valorActual)
+  const sumaLista = lista.reduce((valorAcumulado, valorActual) => valorAcumulado + valorActual) // En este ejemplo esta recibien una arrow function anonima
 
   const average = sumaLista / lista.length
 
   console.log(average);
 }
 
+const esPar = (numberList) => {
+  if (numberList.length % 2 == 0) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(esPar);
+
+function calcularMediana(listaMediana) {
+  const listaEsPar = esPar(listaMediana)
+
+  if (listaEsPar) {
+    const numerosMitadLista = listaMediana.slice((listaMediana.length / 2) - 1, (listaMediana.length / 2) + 1).map(Number)
+    const indexMitadListaPar = calcularPromedio(numerosMitadLista)
+    console.log(indexMitadListaPar);
+  } else {
+    const indexMitadListaImpar =  Math.floor(listaMediana.length / 2)
+    const medianaImpar = listaMediana[indexMitadListaImpar]
+    console.log(medianaImpar);
+  }
+}
