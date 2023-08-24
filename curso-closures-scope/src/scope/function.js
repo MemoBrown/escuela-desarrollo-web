618bae6e06cbcfc68773c42e65fdf72545b2b102
Guillermo Moreno <guillermolpzm@gmail.com>
2023-08-23 19:31:29 -0600

    functions

---
+++ curso-closures-scope/src/scope/function.js
@@ -0,0 +1,12 @@
+function greeting() {
+  let userName = "Gabriela"
+  console.log(userName);
+
+  if (userName === "Gabriela") {
+    console.log(`Hello ${userName}`);
+  }
+
+}
+
+greeting()
+console.log(userName);
